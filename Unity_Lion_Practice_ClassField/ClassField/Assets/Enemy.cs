5d7e9ba5b6c6b1d36f7e911b3c844ca4675b5ae1
661266 <a0989136675@gmail.com>
2020-04-09 13:17:25 +0800

    Unity 練習

---
+++ Unity_Lion_Practice_ClassField/ClassField/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("血量")]
+    [Tooltip("怪物的血量")]
+    [Range(50, 500)]
+    public int hp;
+
+    [Header("魔力")]
+    [Tooltip("怪物的魔力")]
+    [Range(10, 100)]
+    public int mp = 100;
+   
+    [Range(1, 50.5f)]
+    [Header("能力值")]
+    public float speed = 10.5f;
+    
+
+    [Range(20, 200)]
+    public int atk = 50;
+    
+    [Range(0, 100)]
+    public int def;
+
+    [Header("裝備")]
+    public string arms;
+
+    public string 皮衣;
+
+    [Header("其他資料"), Tooltip("怪物是否帶有鑰匙")]
+    public bool key = false;
+
+    [Tooltip("怪物是否帶有寶物")]
+    public bool box = false;
+    
+
+}
---
+++ Unity_Lion_Practice_ClassField/ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    [Header("怪物1號")]
+    public Enemy Monster1;
+
+    [Header("怪物2號")]
+    public Enemy Monster2;
+
+    private void Start()
+    {
+        
+        Monster1.mp = 100;
+        Monster2.hp = 300;
+        Monster1.def = 50;
+        Monster1.arms = "木棍";
+        Monster2.arms = "短刀";
+        Monster1.key = true;
+        Monster2.box = true;
+
+        print("怪物1號的mp:" + Monster1.mp);
+        print("怪物2號的arms:" + Monster2.arms);
+    }
+
+}
+
